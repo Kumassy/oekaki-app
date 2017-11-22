af1b28bac0a277192611df9d5db56152041a0e11
Kumassy <kumassyii@gmail.com>
2017-11-22 20:25:08 +0900

    impl simple api server

---
+++ server/data/model.md
@@ -0,0 +1,39 @@
+# ObjGen
+http://www.objgen.com/json
+
+## post
+http://www.objgen.com/json/models/HkS
+```
+posts[0]
+  id n =1
+  text=textbody
+  image=image.png
+
+posts[1]
+  id n =2
+  text=aasdfbody
+  image=image.jpg
+
+posts[2]
+  id n =3
+  text=ssssfdsfsdf
+  image=fsdfsdds.jpg
+```
+
+## user
+```
+users[0]
+  id n =1
+  name=pandaman
+  avatar=pandaman.png
+
+users[1]
+  id n =2
+  name=kumassy
+  avatar=kumassy.png
+
+users[2]
+  id n =3
+  name=furikake
+  avatar=furikake.png
+```
--- server/index.php
+++ server/index.php
@@ -8,14 +8,38 @@ $app->path('/', function($request) {
 });
 
 $app->path('users', function($request) use($app) {
-  $app->get(function() {
-    return "User page";
-  });
-  $app->path('admin', function($request) use($app) {
-    $app->get(function() {
-      return "Admin page";
-    });
+  $json = file_get_contents('data/users.json');
+  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+  $users = json_decode($json,true);
 
+  $app->get(function($request, $id) use($users) {
+    return $users;
+  });
+  $app->param('int', function($request, $id) use($app, $users) {
+    $app->get(function($request) use($users, $id) {
+      return $users['users'][$id];
+    });
+  });
+  // $app->path('admin', function($request) use($app) {
+  //   $app->get(function() {
+  //     return "Admin page";
+  //   });
+  //
+  // });
+});
+
+$app->path('posts', function($request) use($app) {
+  $json = file_get_contents('data/posts.json');
+  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
+  $posts = json_decode($json,true);
+
+  $app->get(function($request, $id) use($posts) {
+    return $posts;
+  });
+  $app->param('int', function($request, $id) use($app, $posts) {
+    $app->get(function($request) use($posts, $id) {
+      return $posts['posts'][$id];
+    });
   });
 });
 
