af403278d75eb0e5a56978eccd3669af4f5f8eaa
Kumassy <kumassyii@gmail.com>
2017-11-23 18:53:58 +0900

    post components, ajax

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -6,7 +6,27 @@ import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
+} from 'react-router-dom';
+import axios from 'axios';
+
+const _posts = [
+  {
+    'id': 1,
+    'text': 'textbody',
+    'image': 'image.png'
+  },
+  {
+    'id': 2,
+    'text': 'aasdfbody',
+    'image': 'image.jpg'
+  },
+  {
+    'id': 3,
+    'text': 'ssssfdsfsdf',
+    'image': 'fsdfsdds.jpg'
+  }
+];
+const _host = 'http://localhost:3000';
 
 const Home = () => (
   <div>
@@ -54,6 +74,89 @@ const Topics = ({ match }) => (
   </div>
 )
 
+class PostComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        post: {}
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData(this.props.match.params.id);
+  }
+  componentWillReceiveProps(nextProps) {
+    if (this.props.match.params.id !== nextProps.match.params.id) {
+      this.fetchData(nextProps.match.params.id);
+    }
+  }
+
+  fetchData(id) {
+    // return _posts[id];
+    axios.get(`${_host}/posts/${id}`).then((json) => {
+      // return json.data.posts;
+      console.log(this.state);
+      this.setState({
+        post: json.data
+      });
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    // const post = this.fetchData(this.props.match.params.id);
+    const post = this.state.post;
+
+    return (
+      <div>
+        <ul>
+          <li>{post.id}</li>
+          <li>{post.text}</li>
+          <li>{post.image}</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+class PostsComponent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+        posts: []
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    axios.get(`${_host}/posts`).then((json) => {
+      // return json.data.posts;
+      console.log(this.state);
+      this.setState({
+        posts: json.data.posts
+      });
+      console.log(this.state);
+    });
+  }
+
+  render() {
+    const match = this.props.match;
+    return (
+      <div>
+        <h2>Posts</h2>
+        <ul>
+          {this.state.posts.map(post => <li key={post.id}><Link to={`${match.url}/${post.id}`}>{post.text}</Link></li>)}
+        </ul>
+
+        <Route path={`${match.url}/:id`} component={PostComponent}/>
+      </div>
+    )
+  }
+}
+
 class AppComponent extends React.Component {
   render() {
     return (
@@ -67,6 +170,7 @@ class AppComponent extends React.Component {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/topics">Topics</Link></li>
+            <li><Link to="/posts">Posts</Link></li>
           </ul>
 
           <hr/>
@@ -74,6 +178,7 @@ class AppComponent extends React.Component {
           <Route exact path="/" component={Home}/>
           <Route path="/about" component={About}/>
           <Route path="/topics" component={Topics}/>
+          <Route path="/posts" component={PostsComponent}/>
         </div>
       </Router>
     );
