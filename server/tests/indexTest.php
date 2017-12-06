f1815b8dbb321ec16cb69ba179f49264ab5b530e
Kumassy <kumassyii@gmail.com>
2017-12-07 01:00:05 +0900

    update db test

--- server/tests/indexTest.php
+++ server/tests/indexTest.php
@@ -3,59 +3,48 @@
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/app.php';
 
-// use \PHPUnit\Framework\TestCase;
-// use \PHPUnit\DbUnit\TestCaseTrait;
-// use \PHPUnit\DbUnit\DataSet\YamlDataSet;
-
-// require_once "\PHPUnit/Extensions/Database/TestCase.php";
-// require_once "\PHPUnit/Extensions/Database/DataSet/YamlDataSet.php";
-
 require_once __DIR__ . '/DatabaseTestBase.php';
 
 // class DatabaseTest extends TestCase
 // PHPUnit_TestCase
 class DatabaseTest extends DatabaseTestBase
 {
-  // use TestCaseTrait;
-
-  // /**
-  // * @return PHPUnit_Extensions_Database_DB_IDatabaseConnection
-  // */
-  // public function getConnection()
-  // {
-  //   $pod = new PDO($GLOBALS['DB_CONNECT']);
-  //   return $this->createDefaultDBConnection($pod, $GLOBALS['DB_DBNAME']);
-  // }
-  // /**
-  // * @return PHPUnit_Extensions_Database_DataSet_IDataSet
-  // */
-  // public function getDataSet()
-  // {
-  //   // return new YamlDataSet(__DIR__ .'/seed.yml');
-  //   return new PHPUnit_Extensions_Database_DataSet_YamlDataSet(__DIR__ .'/seed.yml');
-  // }
-
-
   public function setUp()
   {
     parent::setUp();
   }
 
-  public function testDB()
+  public function testUserRowCount()
   {
-    // $rows = $this->getConnection()->createQueryTable('users', 'SELECT * from users');
-    // file_put_contents('php://stderr', print_r($rows, TRUE));
-    // $data = $this->getConnection()->createDataSet();
-    // file_put_contents('php://stderr', print_r($data, TRUE));
+    $this->assertEquals(2, $this->getConnection()->getRowCount('users'), "Pre-Condition");
+  }
+
+  public function testSelectUser()
+  {
     $stmt = $this->getPOD()->query('SELECT * FROM users');
     $users = array();
     foreach ($stmt as $user) {
-        $users[] = $user;
+      $u = array();
+      $u['id'] = $user['id'];
+      $u['login_name'] = $user['login_name'];
+      $users[] = $u;
     }
-    file_put_contents('php://stderr', print_r($stmt, TRUE));
-    file_put_contents('php://stderr', print_r($users, TRUE));
-    $this->assertEquals(2, $this->getConnection()->getRowCount('users'), "Pre-Condition");
+    $json = json_encode($users);
 
+
+    $expectedJson = <<<EOD
+[
+  {
+    "id": 1,
+    "login_name": "aaaaa"
+  },
+  {
+    "id": 2,
+    "login_name": "bbbbb"
+  }
+]
+EOD;
+    $this->assertJsonStringEqualsJsonString($expectedJson, $json);
   }
 
 
