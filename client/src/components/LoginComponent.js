58c4ba4f712e7f5c15fd0519ea001f642bbcb7d3
Kumassy <kumassyii@gmail.com>
2018-02-19 20:59:06 +0900

    login with twitter

--- client/src/components/LoginComponent.js
+++ client/src/components/LoginComponent.js
@@ -23,12 +23,12 @@ class LoginComponent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.doSignIn = this.doSignIn.bind(this);
-    this.doSignUp = this.doSignUp.bind(this);
-    this.doSignOut = this.doSignOut.bind(this);
-    this.handleModeChange = this.handleModeChange.bind(this);
-    this.handleUsernameChange = this.handleUsernameChange.bind(this);
-    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    // this.doSignIn = this.doSignIn.bind(this);
+    // this.doSignUp = this.doSignUp.bind(this);
+    // this.doSignOut = this.doSignOut.bind(this);
+    // this.handleModeChange = this.handleModeChange.bind(this);
+    // this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    // this.handlePasswordChange = this.handlePasswordChange.bind(this);
   }
 
   // onSubmit(e) {
@@ -42,121 +42,64 @@ class LoginComponent extends React.Component {
   //   // }
   //   this.doSignIn();
   // }
-
-  componentWillMount() {
-    const { dispatch } = this.props;
-    dispatch(fetchLoggedInUser());
-  }
-
-  doSignIn(e) {
-    e.preventDefault();
-
-    const { dispatch, loginInput } = this.props;
-    const credentials = {
-      'username': loginInput.username,
-      'password': loginInput.password
-    }
-    dispatch(trySignIn(credentials));
-  }
-  doSignUp(e) {
-    e.preventDefault();
-
-    const { dispatch, loginInput } = this.props;
-    const credentials = {
-      'username': loginInput.username,
-      'password': loginInput.password
-    }
-    dispatch(trySignUp(credentials));
-  }
-  doSignOut(e) {
-    e.preventDefault();
-
-    const { dispatch } = this.props;
-    dispatch(trySignOut());
-  }
-
-  handleUsernameChange(e, username) {
-    const { dispatch } = this.props;
-    dispatch(loginInputChanged({
-      username
-    }));
-  }
-  handlePasswordChange(e, password) {
-    const { dispatch } = this.props;
-    dispatch(loginInputChanged({
-      password
-    }));
-  }
-
-  handleModeChange(mode) {
-    const { dispatch } = this.props;
-    dispatch(switchSignInMode(mode));
-  }
+  //
+  // componentWillMount() {
+  //   const { dispatch } = this.props;
+  //   dispatch(fetchLoggedInUser());
+  // }
+  //
+  // doSignIn(e) {
+  //   e.preventDefault();
+  //
+  //   const { dispatch, loginInput } = this.props;
+  //   const credentials = {
+  //     'username': loginInput.username,
+  //     'password': loginInput.password
+  //   }
+  //   dispatch(trySignIn(credentials));
+  // }
+  // doSignUp(e) {
+  //   e.preventDefault();
+  //
+  //   const { dispatch, loginInput } = this.props;
+  //   const credentials = {
+  //     'username': loginInput.username,
+  //     'password': loginInput.password
+  //   }
+  //   dispatch(trySignUp(credentials));
+  // }
+  // doSignOut(e) {
+  //   e.preventDefault();
+  //
+  //   const { dispatch } = this.props;
+  //   dispatch(trySignOut());
+  // }
+  //
+  // handleUsernameChange(e, username) {
+  //   const { dispatch } = this.props;
+  //   dispatch(loginInputChanged({
+  //     username
+  //   }));
+  // }
+  // handlePasswordChange(e, password) {
+  //   const { dispatch } = this.props;
+  //   dispatch(loginInputChanged({
+  //     password
+  //   }));
+  // }
+  //
+  // handleModeChange(mode) {
+  //   const { dispatch } = this.props;
+  //   dispatch(switchSignInMode(mode));
+  // }
 
   render() {
-    const { status, user, loginInput } = this.props;
+    // const { status, user, loginInput } = this.props;
     return (
       <div className="login-component">
-        <Tabs
-          value={status.mode}
-          onChange={this.handleModeChange}
-        >
-          <Tab label="Sign In" value="signin">
-            <div>
-              <form>
-                <TextField
-                  hintText=""
-                  floatingLabelText="ユーザー名"
-                  errorText={loginInput.invalidReasonUsername}
-                  type="text"
-                  onChange={this.handleUsernameChange}
-                  value={loginInput.username}
-                /><br />
-                <TextField
-                  hintText=""
-                  floatingLabelText="パスワード"
-                  errorText={loginInput.invalidReasonPassword}
-                  type="password"
-                  onChange={this.handlePasswordChange}
-                  value={loginInput.password}
-                /><br />
-                <FlatButton
-                  label="Submit"
-                  disabled={!loginInput.isValid}
-                  onClick={this.doSignIn} />
-              </form>
-            </div>
-          </Tab>
-          <Tab label="Sign Up" value="signup">
-            <div>
-              <form>
-                <TextField
-                  hintText=""
-                  floatingLabelText="ユーザー名"
-                  errorText={loginInput.invalidReasonUsername}
-                  type="text"
-                  onChange={this.handleUsernameChange}
-                  value={loginInput.username}
-                /><br />
-                <TextField
-                  hintText=""
-                  floatingLabelText="パスワード"
-                  errorText={loginInput.invalidReasonPassword}
-                  type="password"
-                  onChange={this.handlePasswordChange}
-                  value={loginInput.password}
-                /><br />
-                <FlatButton
-                  label="Submit"
-                  disabled={!loginInput.isValid}
-                  onClick={this.doSignUp} />
-              </form>
-            </div>
-          </Tab>
-        </Tabs>
-        <div className="error-message">
-          {status.error.message}
-        </div>
+        <ul>
+          <li><a href="/auth/twitter">Twitter でログイン</a></li>
+        </ul>
       </div>
     );
   }
